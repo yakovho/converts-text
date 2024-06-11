e5006d1ad6423de9ea5bd0169585c96efaa5fa45
unknown <yakovhorwitz1@gmail.com>
2024-06-11 09:06:54 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,85 @@
+import React, { useState, useRef } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import Swal from 'sweetalert2';
+
+
+function App() {
+  const [type_system, setType_system] = useState("webenefit");
+  const [file, setFile] = useState();
+  const tableRef = useRef(null);
+
+  const handleInputChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+  const upload = () => {
+    if (file) {
+      let formData = new FormData();
+      formData.append("type_system", type_system);
+      formData.append("file.txt", file);
+      axios({
+        method: "post",
+        url: "http://localhost:5000/upload_file",
+        data: formData,
+        responseType: "blob",
+      }).then((response) => {
+
+        // create file link in browser's memory
+        const href = URL.createObjectURL(response.data);
+
+        // create "a" HTML element with href to file & click
+        const link = document.createElement('a');
+        link.href = href;
+        link.setAttribute('download', 'BKMVDATA.txt'); //or any other extension
+        document.body.appendChild(link);
+        link.click();
+
+        // clean up "a" element & remove ObjectURL
+        document.body.removeChild(link);
+        URL.revokeObjectURL(href);
+
+        Swal.fire({
+          title: 'ההמרה בוצעה בהצלחה',
+          text: 'הקובץ החדש ממתין לכם בתיקיית ההורדות',
+          icon: 'success',
+          confirmButtonText: 'תודה',
+          confirmButtonColor: '#2fc1a9'
+        })
+      });
+    }
+    else {
+      Swal.fire({
+        title: 'לא נבחר קובץ להמרה',
+        text: '',
+        icon: 'error',
+        confirmButtonText: 'סגור',
+        confirmButtonColor: '#2fc1a9'
+      })
+    }
+  }
+
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+          <Col sm={4}></Col>
+          <Col sm={4}>
+            <div className='border_form'>
+              <img src='logo.png' width={140}></img>
+              <div className="text_label">בחרו את התוכנה ממנה תרצו לייבא</div>
+              <Form.Select aria-label="Default select example">
+                <option>וובנפיט - webenefit</option>
+              </Form.Select>
+              <div className="text_label">בחרו את הקובץ אחיד בשם BKMVDATA</div>
+              <Form.Control type="file" onChange={handleInputChange} name='webenefit' />
+              <Button onClick={upload}>המר לקובץ אחיד של פינבוט</Button>
+            </div>
+          </Col>
+          <Col sm={4}></Col>
+        </Row>
+      </Container>
+    </div >);
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const path = require('path');
+const http = require('http')
+const PORT = process.env.PORT || 5000
+const app = express()
+const server = http.createServer(app)
+const cors = require('cors');
+const fs = require('fs')
+const multer = require('multer')
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const iconv = require('iconv-lite');
+
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//במידה ואני רוצה להגיש את הצד לקוח דרך השרת
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.post('/upload_file', upload.single('file.txt'), function (req, res) {
+  const type_system = req.body.type_system;
+  console.log(type_system);
+  const file = req.file;
+  if (file) {
+    const multerText = iconv.decode(Buffer.from(file.buffer), 'ISO-8859-8');
+    data_extracted = multerText.split("\r\n");
+
+    if (type_system == "webenefit") {
+      for (let i = 0; i < data_extracted.length; i++) {
+        if (data_extracted[i].slice(0, 4) == 'B100') {
+          //  console.log(data_extracted[i].slice(202, 203))
+          if (data_extracted[i].slice(36, 37) == '1') {
+            if (data_extracted[i].slice(202, 203) == '1' && data_extracted[i + 1].slice(202, 203) == '2') {
+
+              if (data_extracted[i + 2].slice(36, 37) == '3') {
+                let temp = data_extracted[i];
+                data_extracted[i] = data_extracted[i + 1];
+                data_extracted[i + 1] = data_extracted[i + 2];
+                data_extracted[i + 2] = temp;
+              }
+
+              else if (data_extracted[i + 2].slice(36, 37) == '1' || data_extracted[i + 2].slice(36, 37) == '2') {
+                let temp = data_extracted[i];
+                data_extracted[i] = data_extracted[i + 1];
+                data_extracted[i + 1] = temp;
+              }
+
+            }
+          }
+        }
+      }
+    }
+
+    let invoice;
+
+    for (let i = 0; i < data_extracted.length; i++) {
+      invoice += (data_extracted[i]);
+      invoice += "\r\n";
+    }
+
+    const encodedData = iconv.encode(invoice, 'ISO-8859-8');
+
+    fs.writeFile('images/temp.txt', encodedData, err => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.sendFile(path.join(__dirname + '/images/temp.txt'));
+
+        // file written successfully
+        console.log("success");
+      }
+    });
+  }
+  else (res.send("יש לצרף קובץ"));
+});
+
+server.listen(PORT, err => {
+  if (err) console.log(err)
+  console.log('Server running on Port ', PORT)
+})
